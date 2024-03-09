efa0d076d541b2cbac1ac2f9f90f570a1e29e3d9
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-03-09 13:28:14 +0200

    try

--- console.py
+++ console.py
@@ -146,12 +146,12 @@ class HBNBCommand(cmd.Cmd):
             if words[0] not in storage.classes():
                 print("** class doesn't exist **")
             else:
-                l = [str(obj) for key, obj in storage.all().items()
+                lan = [str(obj) for key, obj in storage.all().items()
                      if type(obj).__name__ == words[0]]
-                print(l)
+                print(lan)
         else:
-            l = [str(obj) for key, obj in storage.all().items()]
-            print(l)
+            lan = [str(obj) for key, obj in storage.all().items()]
+            print(lan)
 
     def do_count(self, line):
         """Counts the instances of a class.
