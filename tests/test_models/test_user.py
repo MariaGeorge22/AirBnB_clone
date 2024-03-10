ca508c2c3e3c78a5f30001869edb52ec3d580862
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-03-11 00:29:01 +0200

    user test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""Unittest module for the User Class."""
+""" User Model """
 
 import unittest
-from datetime import datetime
-import time
 from models.user import User
-import re
-import json
 from models.engine.file_storage import FileStorage
 import os
 from models import storage
@@ -15,25 +11,38 @@ from models.base_model import BaseModel
 
 class TestUser(unittest.TestCase):
 
-    """Test Cases for the User class."""
+    """User Class"""
 
     def setUp(self):
-        """Sets up test methods."""
+        """
+        Sets up the test environment before each test method is executed.
+        This method is called automatically by the testing framework.
+        """
         pass
 
     def tearDown(self):
-        """Tears down test methods."""
+        """
+        Cleans up the test environment after each test method is executed.
+        This method is called automatically by the testing framework.
+        """
         self.resetStorage()
         pass
 
     def resetStorage(self):
-        """Resets FileStorage data."""
+        """
+        Resets the FileStorage data to its initial state.
+        """
         FileStorage._FileStorage__objects = {}
         if os.path.isfile(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_8_instantiation(self):
-        """Tests instantiation of User class."""
+        """
+        Tests the instantiation of the User class.
+
+        It checks if an instance of the User class is created correctly
+        and if it inherits from BaseModel.
+        """
 
         b = User()
         self.assertEqual(str(type(b)), "<class 'models.user.User'>")
@@ -41,12 +50,17 @@ class TestUser(unittest.TestCase):
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_8_attributes(self):
-        """Tests the attributes of User class."""
+        """
+        Tests the attributes of the User class.
+
+        It verifies if all attributes specified in the storage are
+        present in an instance of the User class.
+        """
         attributes = storage.attributes()["User"]
-        o = User()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+        obj = User()
+        for key, value in attributes.items():
+            self.assertTrue(hasattr(obj, key))
+            self.assertEqual(type(getattr(obj, key, None)), value)
 
 
 if __name__ == "__main__":
