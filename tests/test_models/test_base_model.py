f8da8440eea1688864796248187c918c8d7c39de
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-03-11 00:59:00 +0200

    git last

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Test suite for the BaseModel class in the models module
-"""
+""" Base model """
 import unittest
 import os
 import models
@@ -12,14 +10,25 @@ from uuid import uuid4
 
 
 class TestMyBaseModel(unittest.TestCase):
-    """
-    Test cases for the BaseModel class
-    """
+    """ base class """
     def test_instance_has_id(self):
+        """
+        Tests if an instance of BaseModel has the attribute 'id'.
+
+        Raises:
+            AssertionError: If the instance does not have the attribute 'id'.
+        """
         my_instance = BaseModel()
         self.assertTrue(hasattr(my_instance, "id"))
 
     def test_string_representation(self):
+        """
+        Tests the string representation of BaseModel instances.
+
+        Raises:
+            AssertionError: If the string representation
+            does not match the expected format.
+        """
         my_instance = BaseModel()
         self.assertEqual(str(my_instance),
                          "[BaseModel] ({}) {}".format(my_instance.id,
@@ -27,7 +36,10 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_unique_ids_generation(self):
         """
-        Validates if IDs are generated randomly and are unique
+        Check the valid ID
+
+        Raises:
+            AssertionError: If IDs generated for two instances are equal.
         """
         instance1 = BaseModel()
         instance2 = BaseModel()
@@ -35,23 +47,42 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_id_type_is_str(self):
         """
-        Verifies that the generated ID is of string type
+        chack if ID is STR
+
+        Raises:
+            AssertionError: If the ID type is not a string.
         """
         instance = BaseModel()
         self.assertTrue(type(instance.id) is str)
 
     def test_created_at_is_datetime_object(self):
         """
-        Ensures that the 'created_at' attribute is a datetime object
+        Raises:
+            AssertionError: If the 'created_at' attribute is not a datetime object.
         """
         instance = BaseModel()
         self.assertTrue(type(instance.created_at) is datetime)
 
     def test_updated_at_is_datetime_object(self):
+        """
+        Ensures that the 'updated_at' attribute is a datetime object.
+
+        Raises:
+            AssertionError: If the 'updated_at'
+            attribute is not a datetime object.
+        """
         instance = BaseModel()
         self.assertTrue(type(instance.updated_at) is datetime)
 
     def test_created_at_difference_between_two_models(self):
+        """
+        Tests if the 'created_at' attribute of one
+        model instance is less than another.
+
+        Raises:
+            AssertionError: If the 'created_at' attribute
+            of the first model is not less than the second model.
+        """
         model1 = BaseModel()
         sleep(0.02)
         model2 = BaseModel()
@@ -59,17 +90,36 @@ class TestMyBaseModel(unittest.TestCase):
         self.assertLess(model1.created_at, model2.created_at)
 
     def test_unused_args_attribute(self):
+        """
+        Tests if BaseModel instantiation with None argument
+        doesn't result in None being in the instance attributes.
+
+        Raises:
+            AssertionError: If None is found in the instance attributes.
+        """
         model = BaseModel(None)
         self.assertNotIn(None, model.__dict__.values())
 
     def test_attributes(self):
-        """Tests the BaseModel attributes"""
+        """
+        Tests the existence of 'id', 'created_at', and 'updated_at'
+        attributes in a BaseModel instance.
+
+        Raises:
+            AssertionError: If any of the attributes are missing.
+        """
         my_model = BaseModel()
         self.assertTrue(hasattr(my_model, "id"))
         self.assertTrue(hasattr(my_model, "created_at"))
         self.assertTrue(hasattr(my_model, "updated_at"))
 
     def test_save_method_updates_updated_at_attribute(self):
+        """
+        Tests if the 'save' method updates the 'updated_at' attribute.
+
+        Raises:
+            AssertionError: If 'updated_at' is not updated after calling 'save'
+        """
         model = BaseModel()
         model.save()
         self.assertNotEqual(model.created_at, model.updated_at)
@@ -77,20 +127,29 @@ class TestMyBaseModel(unittest.TestCase):
                            model.created_at.microsecond)
 
     def test_to_dict_returns_dict_object(self):
+        """
+        Tests if the 'to_dict' method returns a dictionary object.
+
+        Raises:
+            AssertionError: If the returned value is not a dictionary.
+        """
         model = BaseModel()
         self.assertTrue(type(model.to_dict()) is dict)
 
     def test_contains_class_dunder_method(self):
         """
-        Validates if BaseModel's to_dict() method contains the key '__class__'
+        Raises:
+            AssertionError: If '__class__' key is not found
+            in the dictionary returned by to_dict().
         """
         model = BaseModel()
         self.assertTrue("__class__" in model.to_dict())
 
     def test_created_at_format_in_to_dict(self):
         """
-        Verifies that 'created_at' is stored in ISO format
-        as a string in BaseModel's to_dict()
+        Raises:
+            AssertionError: If the format of 'created_at'
+            in the returned dictionary does not match the ISO format.
         """
         model = BaseModel()
         self.assertEqual(model.to_dict()["created_at"],
@@ -98,8 +157,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_updated_at_format_in_to_dict(self):
         """
-        Verifies that 'updated_at' is stored in ISO format
-        as a string in BaseModel's to_dict()
+        Raises:
+            AssertionError: If the format of 'updated_at'
+            in the returned dictionary does not match the ISO format.
         """
         model = BaseModel()
         self.assertEqual(model.to_dict()["updated_at"],
@@ -107,7 +167,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_correct_number_of_keys_in_to_dict(self):
         """
-        Ensures that to_dict() returns the expected number of keys/values
+        Raises:
+            AssertionError: If the number of keys/values in the
+            returned dictionary does not match the expected count.
         """
         model = BaseModel()
         expected_keys_count = len([key for key in model.__dict__
@@ -116,8 +178,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_generation_of_id_created_at_updated_at_when_kwargs_empty(self):
         """
-        Validates that 'id', 'created_at', and 'updated_at' are automatically
-        generated if they're not passed in kwargs
+        Raises:
+            AssertionError: If any of 'id', 'created_at', or 'updated_at'
+            are not generated automatically.
         """
         empty_dict = {}
         model = BaseModel(**empty_dict)
@@ -127,8 +190,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_creation_from_kwargs(self):
         """
-        Ensures that 'id', 'created_at', and 'updated_at'
-        are correctly assigned when provided in kwargs
+        Raises:
+            AssertionError: If any of 'id', 'created_at', or
+            'updated_at' are not assigned correctly.
         """
         my_kwargs = {"id": uuid4(),
                      "created_at": datetime.utcnow().isoformat(),
@@ -144,8 +208,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_kwargs_over_args(self):
         """
-        Validates that when both args and kwargs are passed,
-        BaseModel ignores args
+        Raises:
+            AssertionError: If BaseModel does not ignore args
+            and sets the values from kwargs correctly.
         """
         current_datetime = datetime.now()
         dt_iso = current_datetime.isoformat()
@@ -156,8 +221,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_additional_kwargs_not_broken(self):
         """
-        Ensures BaseModel does not break when kwargs
-        contain additional attributes
+        Raises:
+            AssertionError: If BaseModel breaks or fails
+            to handle additional attributes in kwargs.
         """
         my_dict = {"id": uuid4(), "created_at": datetime.utcnow().isoformat(),
                    "updated_at": datetime.utcnow().isoformat(),
@@ -167,8 +233,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_storage_new_not_called_with_dict_obj(self):
         """
-        Verifies that storage.new() is not called
-        when BaseModel is created from a dict object
+        Raises:
+            AssertionError: If storage.new() is called when
+            BaseModel is created from a dict object.
         """
         my_dict = {"id": uuid4(), "created_at": datetime.utcnow().isoformat(),
                    "updated_at": datetime.utcnow().isoformat(),
@@ -182,7 +249,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_save_updates_updated_at_attribute(self):
         """
-        Checks that save() method updates the 'updated_at' attribute
+        Raises:
+            AssertionError: If save() method fails to
+            update the 'updated_at' attribute.
         """
         model = BaseModel()
         sleep(0.02)
@@ -192,7 +261,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_save_updates_twice(self):
         """
-        Tests that the save method updates 'updated_at' two times
+        Raises:
+            AssertionError: If 'updated_at' is not
+            updated twice by the save method.
         """
         model = BaseModel()
         sleep(0.02)
@@ -207,7 +278,8 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_save_updates_file(self):
         """
-        Tests if the file is updated when 'save' is called
+        Raises:
+            AssertionError: If the file is not updated when 'save' is called.
         """
         model = BaseModel()
         model.save()
@@ -217,7 +289,8 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_correct_keys_in_to_dict(self):
         """
-        Verifies that to_dict() returns the expected keys
+        Raises:
+            AssertionError: If to_dict() does not return the expected keys.
         """
         model_dict = BaseModel().to_dict()
         expected_keys = ("id", "created_at", "updated_at", "__class__")
@@ -226,7 +299,8 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_additional_attributes_in_to_dict(self):
         """
-        Checks that new attributes are also returned by to_dict()
+        Raises:
+            AssertionError: If new attributes are not returned by to_dict().
         """
         model = BaseModel()
         model.name = "Leah"
@@ -238,7 +312,9 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_to_dict_output(self):
         """
-        Verifies the output returned by to_dict()
+        Raises:
+            AssertionError: If the output returned by to_dict()
+            does not match the expected output.
         """
         model = BaseModel()
         current_datetime = datetime.now()
@@ -254,17 +330,15 @@ class TestMyBaseModel(unittest.TestCase):
 
     def test_to_dict_with_args_raises_type_error(self):
         """
-        Checks that TypeError is raised when an argument is passed to to_dict()
+        Raises:
+            AssertionError: If TypeError is not raised
+            when an argument is passed to to_dict().
         """
         model = BaseModel()
         with self.assertRaises(TypeError):
             model.to_dict(None)
 
     def test_to_dict_not_equal_to_dunder_dict(self):
-        """
-        Verifies that to_dict() is a dictionary object
-        and not equal to __dict__
-        """
         model = BaseModel()
         self.assertNotEqual(model.to_dict(), model.__dict__)
 
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""file storage test cases """
+"""base model """
 import os.path
 import unittest
 
