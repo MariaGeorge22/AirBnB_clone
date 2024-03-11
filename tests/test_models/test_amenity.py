fdbbf82ebaff493c063832c91227ebbe4563bc95
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-03-11 16:01:57 +0200

    try a

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -10,7 +10,6 @@ class TestAmenity(unittest.TestCase):
 
     def setUp(self):
         """Set up the test environment"""
-        # Creating an instance of AmenityClass for testing
         self.amenity = Amenity()
 
     def test_amenity_is_a_subclass_of_base_model(self):
@@ -18,11 +17,9 @@ class TestAmenity(unittest.TestCase):
 
     def TestAttr_isA_ClassAttr(self):
         """Test if 'name' is a class attribute of AmenityClass"""
-        # Checking if 'name' is a class attribute of AmenityClass
         self.assertTrue(hasattr(self.amenity, "name"))
 
     def TestClassAttr(self):
         """Test the properties of the 'name' class attribute"""
-        # Checking the type and initial value of the 'name' attribute
         self.assertIs(type(self.amenity.name), str)
         self.assertFalse(bool(getattr(self.amenity, "name")))
