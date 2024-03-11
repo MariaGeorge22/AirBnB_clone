d70a286c4907821b657d7059d03a50b39ad35703
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-03-11 16:10:34 +0200

    tryyy

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -9,14 +9,18 @@ class TestAmenity(unittest.TestCase):
     """ Cases that will be tested for Amenity class """
 
     def setUp(self):
+        """Set up the test environment"""
         self.amenity = Amenity()
 
-    def test_amenity_is_a_subclass_of_basemodel(self):
+    def test_amenity_is_a_subclass_of_base_model(self):
+        """Test if Amenity is a subclass of BaseModel"""
         self.assertTrue(issubclass(type(self.amenity), BaseModel))
 
     def test_attr_is_a_class_attr(self):
+        """Test if 'name' is a class attribute of Amenity"""
         self.assertTrue(hasattr(self.amenity, "name"))
 
     def test_class_attr(self):
+        """Test properties of the 'name' class attribute"""
         self.assertIs(type(self.amenity.name), str)
         self.assertFalse(bool(getattr(self.amenity, "name")))
